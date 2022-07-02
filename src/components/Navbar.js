38b70d6e540229b542199b36ecc99b139f1a80c7
sohail2000 <msohail1379@gmail.com>
2022-07-03 01:02:24 +0530

    frontend

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div className="container">
+            <Link to='/'>
+                Workout Buddy
+            </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/components/WorkoutDetails.js
@@ -0,0 +1,36 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+//icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+//date-fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: json })
+    }
+  }
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Number of reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+      <span onClick={handleClick}><FontAwesomeIcon icon={faTrashCan} /></span>
+    </div>
+  )
+}
+
+export default WorkoutDetails
